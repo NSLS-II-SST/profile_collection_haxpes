0b6b643b00aa261e3869ceb00612cd76d5855110
Charles Titus <ctitus@bnl.gov>
2026-06-18 11:00:39 -0400

    Ensuring that soft energy is actually loaded in GUI when modes change

--- scripts/gui-dev.sh
+++ scripts/gui-dev.sh
@@ -5,7 +5,6 @@ set -o xtrace
 pip install -e /home/xf07id1/collection_packages/nbs-core
 pip install -e /home/xf07id1/collection_packages/nbs-bl
 pip install -e /home/xf07id1/collection_packages/nbs-gui
-pip install -e /home/xf07id1/collection_packages/sst_base
-pip install git+https://github.com/cjtitus/bluesky-widgets@worker_weakref
+pip install -e /home/xf07id1/collection_packages/sst-base
 # haxpes is always installed by gui-start
 $(dirname "$0")/gui-start.sh
